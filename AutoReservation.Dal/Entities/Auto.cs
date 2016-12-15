94590af56c0505e30013b848ccb62e94e0983205
Raphael <raphael.joehl@outlook.com>
2016-12-15 13:31:52 +0100

    Add Auto Class

--- AutoReservation.Dal/Entities/Auto.cs
+++ AutoReservation.Dal/Entities/Auto.cs
@@ -4,5 +4,23 @@ using System.Data.Entity;
 
 namespace AutoReservation.Dal.Entities
 {
- 
+    public abstract class Auto
+    {
+        public int Id { get; set; }
+        public string Marke { get; set; }
+        public byte[] RowVersion { get; set; }
+        public int Tagestarif { get; set; }
+        public virtual DbSet<Reservation> Reservationen { get; set; }
+    }
+
+    public class LuxusklasseAuto : Auto
+    {
+        public int BasisTarif { get; set; }
+    }
+
+    public class MittelKlasseAuto : Auto { }
+
+    public class StandardAuto : Auto { }
+
 }
+

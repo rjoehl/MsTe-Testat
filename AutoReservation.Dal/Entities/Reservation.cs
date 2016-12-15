a35c607ddbb5773d22027ee1edb5e60193adf5cb
Raphael <raphael.joehl@outlook.com>
2016-12-15 13:32:04 +0100

    Add Reservation Class

--- AutoReservation.Dal/Entities/Reservation.cs
+++ AutoReservation.Dal/Entities/Reservation.cs
@@ -4,5 +4,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoReservation.Dal.Entities
 {
-    
+    public class Reservation
+    {
+        public int AutoId { get; set; }
+        public DateTime Bis { get; set; }
+        public int KundeId { get; set; }
+        public int ReservationsNr { get; set; }
+        public byte[] RowVersion { get; set; }
+        public DateTime Von { get; set; }
+        public virtual Auto Auto { get; set; }
+        public virtual Kunde Kunde { get; set; }
+    }
+
 }

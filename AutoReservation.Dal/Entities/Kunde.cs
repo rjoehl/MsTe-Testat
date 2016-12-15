57128b1fd1479e6ab251ad5868804bbfcd3846ba
Raphael <raphael.joehl@outlook.com>
2016-12-15 13:32:21 +0100

    Add Kunde Class

--- AutoReservation.Dal/Entities/Kunde.cs
+++ AutoReservation.Dal/Entities/Kunde.cs
@@ -5,5 +5,14 @@ using System.Data.Entity;
 
 namespace AutoReservation.Dal.Entities
 {
-  
+    public class Kunde
+    {
+        public DateTime Geburtsdatum { get; set; }
+        public int Id { get; set; }
+        public string Nachname { get; set; }
+        public byte[] RowVersion { get; set; }
+        public string Vorname { get; set; }
+        public virtual DbSet<Reservation> Reservationen { get; set; }
+    }
+
 }

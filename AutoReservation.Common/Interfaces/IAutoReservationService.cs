2d0b283f97c6b35684daf253a912f9b3e28e1e3b
Raphael <raphael.joehl@outlook.com>
2016-12-16 14:38:37 +0100

    Add Fault Exceptions

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,55 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Common.FaultExceptions;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        List<AutoDto> Autos { [OperationContract] get; }
+        List<KundeDto> Kunden { [OperationContract] get; }
+        List<ReservationDto> Reservationen { [OperationContract] get; }
+
+        [OperationContract]
+        AutoDto GetAutoById(int id);
+
+        [OperationContract]
+        KundeDto GetKundeById(int id);
+
+        [OperationContract]
+        ReservationDto GetReservationByNr(int reservationsNr);
+
+        [OperationContract]
+        AutoDto InsertAuto(AutoDto autoDto);
+
+        [OperationContract]
+        KundeDto InsertKunde(KundeDto kundeDto);
+
+        [OperationContract]
+        ReservationDto InsertReservation(ReservationDto reservationDto);
+
+        [OperationContract]
+        [FaultContract(typeof(OptimisticConcurrencyFaultContract))]
+        AutoDto UpdateAuto(AutoDto autoDto);
+
+        [OperationContract]
+        [FaultContract(typeof(OptimisticConcurrencyFaultContract))]
+        KundeDto UpdateKunde(KundeDto kundeDto);
+
+        [OperationContract]
+        [FaultContract(typeof(OptimisticConcurrencyFaultContract))]
+        ReservationDto UpdateReservation(ReservationDto reservationDto);
+
+        [OperationContract]
+        void DeleteAuto(AutoDto autoDto);
+
+        [OperationContract]
+        void DeleteKunde(KundeDto kundeDto);
+
+        [OperationContract]
+        void DeleteReservation(ReservationDto reservationDto);
+
     }
 }

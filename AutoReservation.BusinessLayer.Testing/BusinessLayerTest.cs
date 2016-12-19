6c413b5eadcc9dbcfc0d555a3b818a44de8e89ca
Viktor Puselja <viktor.puselja@gmail.com>
2016-12-19 15:53:34 +0100

    Implement business layer

--- AutoReservation.BusinessLayer.Testing/BusinessLayerTest.cs
+++ AutoReservation.BusinessLayer.Testing/BusinessLayerTest.cs
@@ -1,7 +1,9 @@
-﻿using AutoReservation.Dal.Entities;
+﻿using AutoReservation.Dal;
+using AutoReservation.Dal.Entities;
 using AutoReservation.TestEnvironment;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace AutoReservation.BusinessLayer.Testing
 {
@@ -31,19 +33,43 @@ namespace AutoReservation.BusinessLayer.Testing
         [TestMethod]
         public void UpdateAutoTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Autos.First();
+            expected.Tagestarif -= 20;
+            Target.UpdateAuto(expected);
+
+            var actual = Target.GetAutoById(expected.Id);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Marke, actual.Marke);
+            Assert.AreEqual(expected.Reservationen, actual.Reservationen);
+            Assert.AreEqual(expected.Tagestarif, actual.Tagestarif);
         }
 
         [TestMethod]
         public void UpdateKundeTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Kunden.First();
+            expected.Vorname += " J.";
+            Target.UpdateKunde(expected);
+
+            var actual = Target.GetKundeById(expected.Id);
+            Assert.AreEqual(expected.Geburtsdatum, actual.Geburtsdatum);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Nachname, actual.Nachname);
+            Assert.AreEqual(expected.Vorname, actual.Vorname);
         }
 
         [TestMethod]
         public void UpdateReservationTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Reservationen.First();
+            expected.Von = expected.Von.Subtract(new TimeSpan(1, 0, 0));
+            Target.UpdateReservation(expected);
+
+            var actual = Target.GetReservationByNr(expected.ReservationsNr);
+            Assert.AreEqual(expected.AutoId, actual.AutoId);
+            Assert.AreEqual(expected.Bis, actual.Bis);
+            Assert.AreEqual(expected.KundeId, actual.KundeId);
+            Assert.AreEqual(expected.Von, actual.Von);
         }
 
     }
--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -1,5 +1,6 @@
 ﻿using AutoReservation.Dal;
 using AutoReservation.Dal.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
@@ -9,7 +10,128 @@ namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
- 
+        public IEnumerable<Auto> Autos
+        {
+            get
+            {
+                using (var context = new AutoReservationContext())
+                {
+                    return context.Autos.ToList();
+                }
+            }
+        }
+
+        public IEnumerable<Kunde> Kunden
+        {
+            get
+            {
+                using (var context = new AutoReservationContext())
+                {
+                    return context.Kunden.ToList();
+                }
+            }
+        }
+
+        public IEnumerable<Reservation> Reservationen
+        {
+            get
+            {
+                using (var context = new AutoReservationContext())
+                {
+                    return context.Reservationen.ToList();
+                }
+            }
+        }
+
+        public Auto GetAutoById(int id)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                var query = from auto in context.Autos
+                            where auto.Id == id
+                            select auto;
+                return query.First();
+            }
+        }
+
+        public Kunde GetKundeById(int id)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                var query = from kunde in context.Kunden
+                            where kunde.Id == id
+                            select kunde;
+                return query.First();
+            }
+        }
+
+        public Reservation GetReservationByNr(int reservationsNr)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                var query = from reservation in context.Reservationen
+                            where reservation.ReservationsNr == reservationsNr
+                            select reservation;
+                return query.First();
+            }
+        }
+
+        public Auto InsertAuto(Auto auto)
+        {
+            return updateEntityAndState(auto, EntityState.Added);
+        }
+
+        public Kunde InsertKunde(Kunde kunde)
+        {
+            return updateEntityAndState(kunde, EntityState.Added);
+        }
+
+        public Reservation InsertReservation(Reservation reservation)
+        {
+            return updateEntityAndState(reservation, EntityState.Added);
+        }
+
+        public Auto UpdateAuto(Auto auto)
+        {
+            return updateEntityAndState(auto, EntityState.Modified);
+        }
+
+        public Kunde UpdateKunde(Kunde kunde)
+        {
+            return updateEntityAndState(kunde, EntityState.Modified);
+        }
+
+        public Reservation UpdateReservation(Reservation reservation)
+        {
+            return updateEntityAndState(reservation, EntityState.Modified);
+        }
+
+        public void DeleteAuto(Auto auto)
+        {
+            updateEntityAndState(auto, EntityState.Deleted);
+        }
+
+        public void DeleteKunde(Kunde kunde)
+        {
+            updateEntityAndState(kunde, EntityState.Deleted);
+        }
+
+        public void DeleteReservation(Reservation reservation)
+        {
+            updateEntityAndState(reservation, EntityState.Deleted);
+        }
+
+        private T updateEntityAndState<T>(T value, EntityState state)
+            where T : class
+        {
+            using (var context = new AutoReservationContext())
+            {
+                context.Entry(value).State = state;
+                context.SaveChanges();
+            }
+
+            return value;
+        }
 
         private static LocalOptimisticConcurrencyException<T> CreateLocalOptimisticConcurrencyException<T>(AutoReservationContext context, T entity)
             where T : class

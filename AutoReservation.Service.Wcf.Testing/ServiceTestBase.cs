8ecc95827db5dddbfba8c9cd4827eeafae4f198e
Viktor Puselja <viktor.puselja@gmail.com>
2016-12-19 17:27:19 +0100

    Implement normal flow in business layer

--- AutoReservation.Service.Wcf.Testing/ServiceTestBase.cs
+++ AutoReservation.Service.Wcf.Testing/ServiceTestBase.cs
@@ -5,6 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.ServiceModel;
+using System.Linq;
 
 namespace AutoReservation.Service.Wcf.Testing
 {
@@ -24,19 +25,19 @@ namespace AutoReservation.Service.Wcf.Testing
         [TestMethod]
         public void GetAutosTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            Assert.AreEqual(3, Target.Autos.Count());
         }
 
         [TestMethod]
         public void GetKundenTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            Assert.AreEqual(4, Target.Kunden.Count());
         }
 
         [TestMethod]
         public void GetReservationenTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            Assert.AreEqual(3, Target.Reservationen.Count());
         }
 
         #endregion
@@ -46,19 +47,41 @@ namespace AutoReservation.Service.Wcf.Testing
         [TestMethod]
         public void GetAutoByIdTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            foreach (var expected in Target.Autos)
+            {
+                var actual = Target.GetAutoById(expected.Id);
+                Assert.AreEqual(expected.AutoKlasse, actual.AutoKlasse);
+                Assert.AreEqual(expected.Basistarif, actual.Basistarif);
+                Assert.AreEqual(expected.Id, actual.Id);
+                Assert.AreEqual(expected.Marke, actual.Marke);
+                Assert.AreEqual(expected.Tagestarif, actual.Tagestarif);
+            }
         }
 
         [TestMethod]
         public void GetKundeByIdTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            foreach (var expected in Target.Kunden)
+            {
+                var actual = Target.GetKundeById(expected.Id);
+                Assert.AreEqual(expected.Geburtsdatum, actual.Geburtsdatum);
+                Assert.AreEqual(expected.Id, actual.Id);
+                Assert.AreEqual(expected.Nachname, actual.Nachname);
+                Assert.AreEqual(expected.Vorname, actual.Vorname);
+            }
         }
 
         [TestMethod]
         public void GetReservationByNrTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            foreach (var expected in Target.Reservationen)
+            {
+                var actual = Target.GetReservationByNr(expected.ReservationsNr);
+                Assert.AreEqual(expected.Auto, actual.Auto);
+                Assert.AreEqual(expected.Bis, actual.Bis);
+                Assert.AreEqual(expected.Kunde, actual.Kunde);
+                Assert.AreEqual(expected.Von, actual.Von);
+            }
         }
 
         #endregion
@@ -89,19 +112,55 @@ namespace AutoReservation.Service.Wcf.Testing
         [TestMethod]
         public void InsertAutoTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.InsertAuto(new AutoDto()
+            {
+                AutoKlasse = AutoKlasse.Luxusklasse,
+                Basistarif = 42,
+                Marke = "The Answer",
+                Tagestarif = 9999
+            });
+
+            var actual = Target.GetAutoById(expected.Id);
+            Assert.AreEqual(expected.AutoKlasse, actual.AutoKlasse);
+            Assert.AreEqual(expected.Basistarif, actual.Basistarif);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Marke, actual.Marke);
+            Assert.AreEqual(expected.Tagestarif, actual.Tagestarif);
         }
 
         [TestMethod]
         public void InsertKundeTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.InsertKunde(new KundeDto()
+            {
+                Geburtsdatum = new DateTime(2000, 1, 1),
+                Nachname = "Hansen",
+                Vorname = "Jürg"
+            });
+
+            var actual = Target.GetKundeById(expected.Id);
+            Assert.AreEqual(expected.Geburtsdatum, actual.Geburtsdatum);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Nachname, actual.Nachname);
+            Assert.AreEqual(expected.Vorname, actual.Vorname);
         }
 
         [TestMethod]
         public void InsertReservationTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.InsertReservation(new ReservationDto()
+            {
+                Auto = Target.Autos.Last(),
+                Bis = new DateTime(2038, 5, 1),
+                Kunde = Target.Kunden.Last(),
+                Von = new DateTime(2016, 12, 19)
+            });
+
+            var actual = Target.GetReservationByNr(expected.ReservationsNr);
+            Assert.AreEqual(expected.Auto, actual.Auto);
+            Assert.AreEqual(expected.Bis, actual.Bis);
+            Assert.AreEqual(expected.Kunde, actual.Kunde);
+            Assert.AreEqual(expected.Von, actual.Von);
         }
 
         #endregion
@@ -111,19 +170,28 @@ namespace AutoReservation.Service.Wcf.Testing
         [TestMethod]
         public void DeleteAutoTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Autos.Count() - 1;
+            Target.DeleteAuto(Target.Autos.Last());
+
+            Assert.AreEqual(expected, Target.Autos.Count());
         }
 
         [TestMethod]
         public void DeleteKundeTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Kunden.Count() - 1;
+            Target.DeleteKunde(Target.Kunden.Last());
+
+            Assert.AreEqual(expected, Target.Kunden.Count());
         }
 
         [TestMethod]
         public void DeleteReservationTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Reservationen.Count() - 1;
+            Target.DeleteReservation(Target.Reservationen.Last());
+
+            Assert.AreEqual(expected, Target.Reservationen.Count());
         }
 
         #endregion
@@ -133,19 +201,43 @@ namespace AutoReservation.Service.Wcf.Testing
         [TestMethod]
         public void UpdateAutoTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Autos.First();
+            expected.Tagestarif -= 20;
+            Target.UpdateAuto(expected);
+
+            var actual = Target.GetAutoById(expected.Id);
+            Assert.AreEqual(expected.Basistarif, actual.Basistarif);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Marke, actual.Marke);
+            Assert.AreEqual(expected.Tagestarif, actual.Tagestarif);
         }
 
         [TestMethod]
         public void UpdateKundeTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Kunden.First();
+            expected.Vorname += " J.";
+            Target.UpdateKunde(expected);
+
+            var actual = Target.GetKundeById(expected.Id);
+            Assert.AreEqual(expected.Geburtsdatum, actual.Geburtsdatum);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Nachname, actual.Nachname);
+            Assert.AreEqual(expected.Vorname, actual.Vorname);
         }
 
         [TestMethod]
         public void UpdateReservationTest()
         {
-            Assert.Inconclusive("Test not implemented.");
+            var expected = Target.Reservationen.First();
+            expected.Von = expected.Von.Subtract(new TimeSpan(1, 0, 0));
+            Target.UpdateReservation(expected);
+
+            var actual = Target.GetReservationByNr(expected.ReservationsNr);
+            Assert.AreEqual(expected.Auto, actual.Auto);
+            Assert.AreEqual(expected.Bis, actual.Bis);
+            Assert.AreEqual(expected.Kunde, actual.Kunde);
+            Assert.AreEqual(expected.Von, actual.Von);
         }
 
         #endregion
--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,14 +1,115 @@
-﻿using System;
+﻿using AutoReservation.Common.Interfaces;
+using System;
 using System.Diagnostics;
+using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using AutoReservation.BusinessLayer;
 
 namespace AutoReservation.Service.Wcf
 {
-    public class AutoReservationService
+    public class AutoReservationService : IAutoReservationService
     {
+        private readonly AutoReservationBusinessComponent component = new AutoReservationBusinessComponent();
+
+        public List<AutoDto> Autos
+        {
+            get
+            {
+                WriteActualMethod();
+                return component.Autos.ConvertToDtos();
+            }
+        }
+
+        public List<KundeDto> Kunden
+        {
+            get
+            {
+                WriteActualMethod();
+                return component.Kunden.ConvertToDtos();
+            }
+        }
+
+        public List<ReservationDto> Reservationen
+        {
+            get
+            {
+                WriteActualMethod();
+                return component.Reservationen.ConvertToDtos();
+            }
+        }
 
         private static void WriteActualMethod()
         {
             Console.WriteLine($"Calling: {new StackTrace().GetFrame(1).GetMethod().Name}");
+        }
+
+        public void DeleteAuto(AutoDto autoDto)
+        {
+            component.DeleteAuto(autoDto.ConvertToEntity());
+        }
+
+        public void DeleteKunde(KundeDto kundeDto)
+        {
+            component.DeleteKunde(kundeDto.ConvertToEntity());
+        }
+
+        public void DeleteReservation(ReservationDto reservationDto)
+        {
+            component.DeleteReservation(reservationDto.ConvertToEntity());
+        }
+
+        public AutoDto GetAutoById(int id)
+        {
+            WriteActualMethod();
+            return component.GetAutoById(id).ConvertToDto();
+        }
+
+        public KundeDto GetKundeById(int id)
+        {
+            WriteActualMethod();
+            return component.GetKundeById(id).ConvertToDto();
+        }
+
+        public ReservationDto GetReservationByNr(int reservationsNr)
+        {
+            WriteActualMethod();
+            return component.GetReservationByNr(reservationsNr).ConvertToDto();
+        }
+
+        public AutoDto InsertAuto(AutoDto autoDto)
+        {
+            WriteActualMethod();
+            return component.InsertAuto(autoDto.ConvertToEntity()).ConvertToDto();
+        }
+
+        public KundeDto InsertKunde(KundeDto kundeDto)
+        {
+            WriteActualMethod();
+            return component.InsertKunde(kundeDto.ConvertToEntity()).ConvertToDto();
+        }
+
+        public ReservationDto InsertReservation(ReservationDto reservationDto)
+        {
+            WriteActualMethod();
+            return component.InsertReservation(reservationDto.ConvertToEntity()).ConvertToDto();
+        }
+
+        public AutoDto UpdateAuto(AutoDto autoDto)
+        {
+            WriteActualMethod();
+            return component.UpdateAuto(autoDto.ConvertToEntity()).ConvertToDto();
+        }
+
+        public KundeDto UpdateKunde(KundeDto kundeDto)
+        {
+            WriteActualMethod();
+            return component.UpdateKunde(kundeDto.ConvertToEntity()).ConvertToDto();
+        }
+
+        public ReservationDto UpdateReservation(ReservationDto reservationDto)
+        {
+            WriteActualMethod();
+            return component.UpdateReservation(reservationDto.ConvertToEntity()).ConvertToDto();
         }
     }
 }

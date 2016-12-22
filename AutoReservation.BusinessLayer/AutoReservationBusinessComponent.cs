9a674dd6b06b1f5dddf5bf4dedad2234cedfa57e
Viktor Puselja <viktor.puselja@gmail.com>
2016-12-22 18:25:13 +0100

    Reorder business layer methods

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -10,6 +10,16 @@ namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
+        private static LocalOptimisticConcurrencyException<T> CreateLocalOptimisticConcurrencyException<T>(AutoReservationContext context, T entity)
+            where T : class
+        {
+            var dbEntity = (T)context.Entry(entity)
+                .GetDatabaseValues()
+                .ToObject();
+
+            return new LocalOptimisticConcurrencyException<T>($"Update {typeof(Auto).Name}: Concurrency-Fehler", dbEntity);
+        }
+
         public IEnumerable<Auto> Autos
         {
             get
@@ -202,16 +212,6 @@ namespace AutoReservation.BusinessLayer
             }
 
             return value;
-        }
-
-        private static LocalOptimisticConcurrencyException<T> CreateLocalOptimisticConcurrencyException<T>(AutoReservationContext context, T entity)
-            where T : class
-        {
-            var dbEntity = (T)context.Entry(entity)
-                .GetDatabaseValues()
-                .ToObject();
-
-            return new LocalOptimisticConcurrencyException<T>($"Update {typeof(Auto).Name}: Concurrency-Fehler", dbEntity);
         }
     }
 }
